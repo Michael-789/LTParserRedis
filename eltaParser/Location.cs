176952002ff1da0244a3e62c75f4f51a4ead91ef
Michael <michael.son.eng@gmail.com>
2023-06-14 13:31:47 +0300

    first change

--- eltaParser/Location.cs
+++ eltaParser/Location.cs
@@ -4,18 +4,18 @@ public class Location
 {
     public int Longitude { get; set; }
     public int Latitude { get; set; }
-    public int Elevation{ get; set; }
+    public int Altitude { get; set; }
 
 
     public Location()
 	{
-        //
+        //  
 	}
 
     public Location(int x, int y, int z)
     {
         this.Longitude = x;
         this.Latitude = y;
-        this.Elevation = z;
+        this.Altitude = z;
     }
 }
